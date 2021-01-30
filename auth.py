adbb20175165699ceda9ed268adba1a78274d720
DESKTOP-72TMIV6\Ahmed Hamouda <a.hamodys@yahoo.com>
2021-01-30 20:05:23 +0200

    deploy

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'ahmedfsnd.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'casting'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 ## AuthError Exception
 '''
